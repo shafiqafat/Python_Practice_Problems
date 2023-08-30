099890eac759035385eb450641209ff41308b8c6
Mohammad Shafiqur Rahman <90058132+shafiqafat@users.noreply.github.com>
2023-08-30 23:10:24 +0600

    Update Collatz Sequence.py

--- Collatz Sequence.py
+++ Collatz Sequence.py
@@ -3,7 +3,6 @@ def collatz(number):
         print(number)
         if number % 2 == 0:         #If the number is even
             number = int(number // 2)
-
         else:                 #If the number is odd
             number = int(3 * number + 1)
     else:
